d5ef5076aa596995d93e5646bb1de08e49fc3343
christofferholmesland <cr.holmesland@stud.uis.no>
2018-11-16 08:01:46 +0100

    Removed pagetype query

--- restaurant/blueprints/group7/payment/routes.py
+++ restaurant/blueprints/group7/payment/routes.py
@@ -6,5 +6,5 @@ payment = Blueprint('payment',__name__, url_prefix="/payment")
 
 @payment.route("/<int:order_id>")
 def index(order_id):
-    response = requests.get("https://dat210payment.azurewebsites.net/payment-pages/%d?page=payment&type=customer" % order_id)
+    response = requests.get("https://dat210payment.azurewebsites.net/payment-pages/%d?page=payment" % order_id)
     return response
