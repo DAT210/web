5f0508d6b308354a679abac18eba34a518e2e0d7
Ole <oav@live.no>
2018-11-21 15:29:20 +0100

    Updated run.py to run in production mode.

--- restaurant/run.py
+++ restaurant/run.py
@@ -2,7 +2,7 @@ from create_app import create_app
 # cross origin resource sharing
 from flask_cors import CORS
 
-app = create_app()
+app = create_app('production')
 CORS(app)
 
 app.run(host="0.0.0.0", port=80)
