8e65b75e4dafba1b42125b26c15efd693e9feb09
David Galante <davidgalante77@gmail.com>
2018-11-30 18:21:06 +0100

    front of  the booking edition

--- restaurant/blueprints/group1/booking/routes.py
+++ restaurant/blueprints/group1/booking/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
 import requests
 
 
@@ -11,5 +11,24 @@ def index():
     restaurantSelection=response.text
     if response.status_code is 200:
         return render_template("/group1/booking.html",restaurantSelection=restaurantSelection)
+    else:
+        return "ERROR " + str(response.content)
+
+@booking.route("/summary",methods=["POST"])
+def summary():
+    response = requests.post("http://127.0.0.1:4001/editpage/bookingsummary", timeout=10,data={'theName': request.form["theName"], 'thePhone': request.form["thePhone"], 'theEmail': request.form["theEmail"], 'theRestaurant': request.form["theRestaurant"],'thePeople': request.form["thePeople"],'theDate': request.form["theDate"],'theTime': request.form["theTime"]})
+    bookingSummary=response.text
+    if response.status_code is 200:
+        return render_template("/group1/bookingSummary.html",bookingSummary=bookingSummary)
+    else:
+        return "ERROR " + str(response.content)
+
+
+@booking.route("/updatebooking",methods=["POST"])
+def updateBooking():
+    response = requests.post("http://127.0.0.1:4001/editpage/updatebooking", timeout=10,data={'theName': request.form["theName"], 'thePhone': request.form["thePhone"], 'theEmail': request.form["theEmail"], 'theRestaurant': request.form["theRestaurant"],'thePeople': request.form["thePeople"],'theDate': request.form["theDate"],'theTime': request.form["theTime"]})
+    updateBooking=response.text
+    if response.status_code is 200:
+        return render_template("/group1/updateBooking.html",updateBooking=updateBooking)
     else:
         return "ERROR " + str(response.content)
